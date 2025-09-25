f844bc5bba8048e93be4bc69a849182967824d33
Alannuril <alannuril013@gmail.com>
2025-09-25 18:38:08 +0700

    modified public src and pages

--- frontend/src/components/Kategori.jsx
+++ frontend/src/components/Kategori.jsx
@@ -10,47 +10,47 @@ const Kategori = () => {
           </h4>
         </div>
         <div className="mt-10">
-          <div className="flex items-center gap-4 max-lg:flex-col">
-            <figure className="relative w-[40%] max-lg:w-full">
+          <div className="flex items-center gap-6 mt-6 max-lg:flex-col">
+            <figure className="relative w-[40%] h-[289px] max-lg:w-full bg-white rounded-[20px]">
               <img
                 src="/dressimg1.png"
                 alt="dress image 1"
-                className="rounded-[20px] max-lg:w-full max-lg:h-[310px] max-lg:object-cover"
+                className="rounded-[20px] w-full h-full object-cover"
               />
-              <span className="text-4xl font-bold  absolute top-5 left-7">
-                Casual
+              <span className="text-4xl text-white font-bold  absolute top-5 left-7 [text-shadow:2px_2px_4px_rgba(0,0,0,0.5)]">
+                Makeup
               </span>
             </figure>
-            <figure className="relative w-[65%] max-lg:w-full">
+            <figure className="relative min-w-[60%] h-[289px] max-lg:w-full">
               <img
                 src="/dressimg2.png"
                 alt="dress image 2"
-                className="rounded-[20px]"
+                className="rounded-[20px] w-full h-full object-cover"
               />
-              <span className="text-4xl font-bold absolute top-5 left-7">
-                Formal
+              <span className="text-4xl text-white font-bold absolute top-5 left-7 [text-shadow:2px_2px_4px_rgba(0,0,0,0.5)]">
+                Fashion
               </span>
             </figure>
           </div>
           <div className="flex items-center gap-6 mt-6 max-lg:flex-col">
-            <figure className="relative w-[60%] h-[289px] max-lg:w-full">
+            <figure className="relative min-w-[60%] h-[289px] max-lg:w-full">
               <img
                 src="/dressimg3.png"
                 alt="dress image 3"
                 className="rounded-[20px] w-full h-full object-cover"
               />
-              <span className="text-4xl font-bold absolute top-5 left-7">
-                Party
+              <span className="text-4xl text-white font-bold absolute top-5 left-7 [text-shadow:2px_2px_4px_rgba(0,0,0,0.5)]">
+                Gym
               </span>
             </figure>
-            <figure className="relative w-[40%] h-[289px] max-lg:w-full bg-white rounded-[20px]">
+            <figure className="relative md:min-w-[30%] xl:min-w-[38%] 2xl:min-w-[39%] h-[289px] max-lg:w-full bg-white rounded-[20px]">
               <img
                 src="/dressimg4.png"
                 alt="dress image 4"
                 className="rounded-[20px] w-full h-full object-cover"
               />
-              <span className="text-4xl font-bold absolute top-5 left-7">
-                Gym
+              <span className="text-4xl text-white font-bold absolute top-5 left-7 [text-shadow:2px_2px_4px_rgba(0,0,0,0.5)]">
+                Furniture
               </span>
             </figure>
           </div>
